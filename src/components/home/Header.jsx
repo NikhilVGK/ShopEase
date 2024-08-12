224f59415d5c9d7133751dbdaad860855a64e897
NikhilVGK <nikhil.vg14@gmail.com>
2024-08-12 23:16:19 +0530

    firstchanges

---
+++ src/components/home/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+
+const Header = () => {
+  return (
+    <div>
+      <div className='header-container'>
+        <div className='header-title'>ShopEase</div>
+      </div>
+      <hr className='header-line'/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from '../components/home/Header'
+import "./home.css"
+import Categories from '../components/home/categories/Categories'
+import  Filter  from '../components/home/filter/Filter'
+
+
+const Home = () => {
+  const HomeData = {
+        title1: "Electronics",
+        title2: "Fashion",
+        title3: "Furniture",
+  }
+  const keyWords = {
+      filter1: "Laptop",
+      filter2: "Pants",
+      filter3: "Phone",
+      filter4: "Jewels",
+      filter5: "Sofa",
+      filter6: "Table",
+  }
+  
+  return (
+    <div className='home-container'>
+     <Header/>
+     <Categories apiData={HomeData}/>
+     <Filter  filter = {keyWords}/>
+    </div>
+  )
+}
+
+export default Home
